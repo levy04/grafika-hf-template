b43f812ff1c265b4d1014d61b8ee4abf5ddbc725
Orbán Levente László <orban.levente.laszlo@gmail.com>
2025-02-26 18:13:06 +0100

    idfk

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 const char* vertSource = R"(
 	#version 330				
 
-	layout(location = 0) in vec2 cP;
+	layout(location = 0) in vec3 cP;
 
 	void main() {
-		gl_Position = vec4(cP, 0, 1);
+		gl_Position = vec4(cP, 1);
 	}
 )";
 
@@ -25,9 +25,9 @@ const char* fragSource = R"(
 
 // Object
 struct Object {
-    Geometry<vec2> points;
+    Geometry<vec3> points;
 
-    void add(vec2 p) { points.Vtx().push_back(p); }
+    void add(vec3 p) { points.Vtx().push_back(p); }
     void sync() { points.updateGPU(); }
     void draw(GPUProgram* prog, int primitive_type, vec3 color) {
         points.Draw(prog, primitive_type, color);
@@ -37,13 +37,13 @@ struct Object {
 
 // PointCollection
 struct PointCollection : public Object {
-    vec2 find_nearest_point(vec2 source) {
-        vec2 nearest = source;
+    vec3 find_nearest_point(vec3 source) {
+        vec3 nearest = points.Vtx()[0];
         float min_distance = 1000000.0;
 
         for (auto&& point : points.Vtx()) {
             float current_dist = distance(source, point);
-            if (current_dist < min_distance && current_dist != 0) {
+            if (current_dist < min_distance) {
                 min_distance = current_dist;
                 nearest = point;
             }
@@ -55,16 +55,18 @@ struct PointCollection : public Object {
 
 // Line
 struct Line : public Object {
-    vec2 line_point;
-    vec2 v;
+    vec3 point_a, point_b;
+    vec3 coords;
 
-    Line(vec2 a, vec2 b, bool print) {
+    Line(vec3 a = vec3(-1, -1, 1), vec3 b = vec3(1, 1, 1), bool print = false) {
         this->add(a);
         this->add(b);
+        point_a = a;
+        point_b = b;
+        coords = cross(a, b);
 
-        line_point = a;
-        v = a - b;          // irányvektor
-        vec2 n(-v.y, v.x);  // normálvektor
+        vec3 v(a.x - b.x, a.y - b.y, 0);  // irányvektor
+        vec3 n(v.y, -v.x, 0);             // normálvektor
         float d = (-1) * a.x * n.x + a.y * n.y;
 
         if (print) {
@@ -74,43 +76,36 @@ struct Line : public Object {
         }
     }
 
-    vec2 intersection_point(Line line_b) { return cross(vec3(), vec3()); }
+    // static vec3 intersection_point(vec3 line_a, vec3 line_b) {
+    //     return cross(vec3(line_a.x, line_a.y, 0), vec3(line_b.x, line_b.y,
+    //     0));
+    // }
 
-    bool contains_point(vec2 point) {
-        vec2 n(-v.y, v.x);
-
-        Line perp(point, point + n, false);
-        vec2 intersect = intersection_point(perp);
-
-        return distance(point, intersect) < 0.01;
+    static vec3 intersection_point(vec3 line_a, vec3 line_b) {
+        vec3 inter = cross(line_a, line_b);
+        return vec3(inter.x / inter.z, inter.y / inter.z, 1);
     }
 
-    std::pair<vec2, vec2> bordering_points() {
-        std::vector<vec2> intersections = {
-            intersection_point(Line(vec2(-1, 1), vec2(-1, -1), false)),
-            intersection_point(Line(vec2(-1, 1), vec2(1, 1), false)),
-            intersection_point(Line(vec2(1, 1), vec2(1, -1), false)),
-            intersection_point(Line(vec2(1, -1), vec2(-1, -1), false)),
-        };
+    bool contains_point(vec3 point) {
+        vec3 point_asdf(point.x + coords.y, point.y - coords.x, 1);
+        vec3 point_fdsa(-point.x - coords.y, -point.y + coords.x, 1);
+        Line perp_line = Line(point_asdf, point_fdsa);
+        vec3 perp = intersection_point(this->coords, perp_line.coords);
 
-        std::vector<vec2> result = {};
+        bool result = distance(point, perp) < 0.01;
+        printf("%s", result ? "\tYes\n" : "\tNo\n");
 
-        for (auto&& intpoint : intersections) {
-            if (intpoint.x >= -1 && intpoint.x <= 1 && intpoint.y >= -1 &&
-                intpoint.y <= 1) {
-                result.push_back(intpoint);
-            }
-        }
-
-        return std::make_pair(result[0], result[1]);
-
-        // return std::make_pair(vec2(-1, -1), vec2(1, 1));
+        return result;
     }
 
-    void push_to_point(vec2 target) {
-        vec2 push = target - line_point;
-        for (auto&& point : this->points.Vtx()) point += push;
-    }
+    // std::pair<vec3, vec3> bordering_points() {
+
+    // }
+
+    // void push_to_point(vec3 target) {
+    //     vec2 push(target.x - )
+    //     for (auto&& point : this->points.Vtx()) point += push;
+    // }
 };
 //
 
@@ -119,19 +114,27 @@ struct LineCollection : public Object {
     std::vector<Line> lines;
 
     void addLine(Line l) {
+        this->add(l.point_a);
+        this->add(l.point_b);
         lines.push_back(l);
-        this->add(l.line_point);
-        this->add(l.line_point - l.v);
     }
 
-    Line get_closest_to_point(vec2 point) {
-        for (auto&& line : lines)
-            if (line.contains_point(point)) return line;
+    vec3 get_closest_to_point(vec3 point) {
+        for (auto& line : lines)
+            if (line.contains_point(point)) return line.coords;
+        return lines[0].coords;
     }
 };
 //
 
-enum t_state { POINTS, LINES_FIRST, LINES_SECOND, MOVE_LINES, INTERSECT };
+enum t_state {
+    POINTS,
+    LINES_FIRST,
+    LINES_SECOND,
+    MOVE_LINES,
+    INTERSECT_FIRST,
+    INTERSECT_SECOND
+};
 
 const int winWidth = 600, winHeight = 600;
 class pointsAndLinesApp : public glApp {
@@ -139,10 +142,12 @@ class pointsAndLinesApp : public glApp {
     PointCollection* pcoll;
     LineCollection* lcoll;
     GPUProgram* gpuProgram;  // csúcspont és pixel árnyalók
-    vec2 line_points[2];
+    vec3 line_points[2];
+    vec3 lines[2];
 
-    vec2 PixelToNDC(int pX, int pY) {
-        return vec2(2.0f * pX / winWidth - 1.0f, 1.0f - 2.0f * pY / winHeight);
+    vec3 PixelToNDC(int pX, int pY) {
+        return vec3(2.0f * pX / winWidth - 1.0f, 1.0f - 2.0f * pY / winHeight,
+                    1);
     }
 
    public:
@@ -178,7 +183,7 @@ class pointsAndLinesApp : public glApp {
                 state = MOVE_LINES;
                 break;
             case 'i':
-                state = INTERSECT;
+                state = INTERSECT_FIRST;
                 break;
             default:
                 break;
@@ -189,7 +194,7 @@ class pointsAndLinesApp : public glApp {
         switch (state) {
             case POINTS:
                 if (button == MOUSE_LEFT) {
-                    vec2 point = PixelToNDC(pX, pY);
+                    vec3 point = PixelToNDC(pX, pY);
                     printf("Point %f, %f added\n", point.x, point.y);
                     pcoll->add(point);
                     pcoll->sync();
@@ -211,11 +216,29 @@ class pointsAndLinesApp : public glApp {
                     state = LINES_FIRST;
 
                     Line line(line_points[0], line_points[1], true);
+                    // std::pair<vec3, vec3> borders = line.bordering_points();
 
-                    std::pair<vec2, vec2> borders = line.bordering_points();
-
-                    lcoll->addLine(Line(borders.first, borders.second, true));
+                    // lcoll->addLine(Line(borders.first, borders.second));
+                    lcoll->addLine(line);
                     lcoll->sync();
+                    refreshScreen();
+                }
+                break;
+
+            case INTERSECT_FIRST:
+                if (button == MOUSE_LEFT) {
+                    lines[0] = lcoll->get_closest_to_point(PixelToNDC(pX, pY));
+                    state = INTERSECT_SECOND;
+                }
+                break;
+
+            case INTERSECT_SECOND:
+                if (button == MOUSE_LEFT) {
+                    lines[1] = lcoll->get_closest_to_point(PixelToNDC(pX, pY));
+                    state = INTERSECT_FIRST;
+
+                    pcoll->add(Line::intersection_point(lines[0], lines[1]));
+                    pcoll->sync();
                     refreshScreen();
                 }
                 break;
